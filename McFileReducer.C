5fb46fdcde95ed8ebde50a518fef58aab289fa4c
Oleksii Lubynets <lubynets@lxbk0552.gsi.de>
2024-10-18 16:40:14 +0200

    add McFileReducer.C

---
+++ McFileReducer.C
@@ -0,0 +1,30 @@
+void McFileReducer() {
+  std::string fileInName = "/lustre/alice/alien/alice/sim/2024/LHC24e3/0/526641/AOD/001/AO2D.root";
+  std::vector<std::string> dataFrameNames{"DF_2261906078830742", "DF_2261906078830870"};
+  std::string fileOutName = "fileOut.root";
+
+  TFile* fileIn = TFile::Open(fileInName.c_str(), "open");
+  TFile* fileOut = TFile::Open(fileOutName.c_str(), "recreate");
+
+  TMap* tmap = (TMap*)fileIn->Get<TMap>("metaData")->Clone();
+  fileOut->cd();
+  tmap->Write("metaData", 1);
+
+  for(auto& dFN : dataFrameNames) {
+    TDirectoryFile* tdf = fileIn->Get<TDirectoryFile>(dFN.c_str());
+    TDirectoryFile* newtdf = new TDirectoryFile(dFN.c_str(), dFN.c_str());
+    fileOut->cd();
+    newtdf->Write();
+    newtdf->cd();
+    TList* lok = tdf->GetListOfKeys();
+    for(const auto& k : *lok) {
+      std::string treeName = k->GetName();
+      TTree* tree = (TTree*)fileIn->Get<TTree>((dFN + "/" + treeName).c_str())->CloneTree();
+      tree->Write();
+    }
+    fileOut->cd();
+  }
+
+  fileOut->Close();
+  fileIn->Close();
+}
