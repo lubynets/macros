88e201a5bf03d1c3110179b1682a32f94aa62949
Oleksii Lubynets <lubynets@lxbk0552.gsi.de>
2024-11-13 20:49:01 +0100

    improve aliCleaner.sh

--- aliCleaner.sh
+++ aliCleaner.sh
@@ -4,18 +4,26 @@ LABLES_TO_SAVE=('latest' 'latest-KF-o2')
 
 cd sw
 TEMPDIR=tempdir
-cd slc9_x86-64
+TARGET_DIR=slc9_x86-64
+cd $TARGET_DIR
 
 for dir in */ ; do
 echo $dir
 cd $dir
 mkdir -p ../../$TEMPDIR/$dir
 for LABEL in ${LABLES_TO_SAVE[@]}; do
-cp -r $LABEL ../../$TEMPDIR/$dir
 REALNAME=`readlink $LABEL`
+mv $LABEL ../../$TEMPDIR/$dir
+echo $REALNAME
 if [ ! -d ../../$TEMPDIR/$dir/$REALNAME ]; then
-cp -r $REALNAME ../../$TEMPDIR/$dir
+echo "mv $REALNAME ../../$TEMPDIR/$dir"
+mv $REALNAME ../../$TEMPDIR/$dir
 fi
 done
 cd ..
 done
+
+cd ..
+
+rm -r $TARGET_DIR
+mv $TEMPDIR $TARGET_DIR
