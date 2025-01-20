bdc323e3349270529e03601ab31af2114f3c2f18
Oleksii Lubynets <O.Lubynets@gsi.de>
2025-01-20 18:29:39 +0100

    start qa2/mc_qa2diff.C

---
+++ qa2/mc_qa2diff.C
@@ -0,0 +1,146 @@
+template<typename T>
+std::string to_string_with_precision(const T a_value, const int n = 6);
+
+std::vector<std::pair<std::string, std::string>> FindCuts(const TFile* fileIn, const std::string& name_start);
+
+struct HistoQuantities {
+  float underflow_{-999.f};
+  float overflow_{-999.f};
+  float mean_{-999.f};
+  float mean_err_{-999.f};
+  float stddev_{-999.f};
+  float stddev_err_{-999.f};
+};
+
+HistoQuantities EvaluateHistoQuantities(const TH1* h);
+TPaveText ConverHistoQuantitiesToText(const HistoQuantities& q, float x1, float y1, float x2, float y2);
+
+void mc_qa2diff(const std::string& fileName, int prompt_or_nonprompt=1) {
+  TString currentMacroPath = __FILE__;
+  TString directory = currentMacroPath(0, currentMacroPath.Last('/'));
+  gROOT->Macro( directory + "/mc_qa2.style.cc" );
+
+  if(prompt_or_nonprompt !=1 && prompt_or_nonprompt != 2) {
+    throw std::runtime_error("prompt_or_nonprompt must be 1 or 2");
+  }
+
+  const std::string promptness = prompt_or_nonprompt == 1 ? "prompt" : "nonprompt";
+
+  TFile* fileIn = TFile::Open(fileName.c_str());
+  if(fileIn == nullptr) {
+    throw std::runtime_error("fileIn == nullptr");
+  }
+
+  std::string fileOutName = "mc_qa2diff";
+
+  struct Variable {
+    std::string name_;
+    std::string cut_name_;
+    std::string cut_title_;
+    std::string cut_unit_;
+    bool log_res_;
+    bool log_pull_;
+  };
+
+  std::vector<Variable> vars {
+//  name                logres logpull
+    {"P",   "psim",  "p^{mc}",     "GeV/c", false, false},
+    {"Pt",  "pTsim", "p_{T}^{mc}", "GeV/c", false, false},
+    {"Xsv", "psim",  "p^{mc}",     "GeV/c", false, false},
+    {"Ysv", "psim",  "p^{mc}",     "GeV/c", false, false},
+    {"Zsv", "psim",  "p^{mc}",     "GeV/c", false, false},
+    {"L",   "lsim",  "L^{mc}",     "cm",    true,  true},
+    {"T",   "tsim",  "T^{mc}",     "ps",    true,  true},
+  };
+
+  bool is_first_canvas{true};
+  std::string printing_bracket = "(";
+  for(auto& var : vars) {
+    TH1D* hres = fileIn->Get<TH1D>(("Candidates_Simulated_"  + promptness + "_total/res_" + var.name_).c_str());
+    TH1D* hpull = fileIn->Get<TH1D>(("Candidates_Simulated_"  + promptness + "_total/pull_" + var.name_).c_str());
+    if(hmc == nullptr || hrec == nullptr || hres == nullptr || hcorr == nullptr || hpull == nullptr) {
+      throw std::runtime_error(("hmc == nullptr || hrec == nullptr || hres == nullptr || hcorr == nullptr || hpull == nullptr for " + var.name_).c_str());
+    }
+
+    hres->UseCurrentStyle();
+    hpull->UseCurrentStyle();
+
+    TPaveText promptnessText(0.74, 0.86, 0.87, 0.90, "brNDC");
+    promptnessText.SetFillColor(0);
+    promptnessText.SetTextSize(0.03);
+    promptnessText.SetTextFont(62);
+    promptnessText.AddText(promptness.c_str());
+
+    TCanvas ccRes("ccRes", "ccRes", 1200, 800);
+    ccRes.SetLogy(var.log_res_);
+    hres->Draw();
+    promptnessText.Draw("same");
+    HistoQuantities res_quant = EvaluateHistoQuantities(hres);
+    TPaveText res_quant_text = ConverHistoQuantitiesToText(res_quant, 0.74, 0.6, 0.87, 0.7);
+    res_quant_text.Draw("same");
+
+    TCanvas ccPull("ccPull", "ccPull", 1200, 800);
+    ccPull.SetLogy(var.log_pull_);
+    hpull->Draw();
+    promptnessText.Draw("same");
+    HistoQuantities pull_quant = EvaluateHistoQuantities(hpull);
+    TPaveText pull_quant_text = ConverHistoQuantitiesToText(pull_quant, 0.74, 0.6, 0.87, 0.7);
+    pull_quant_text.Draw("same");
+
+    if(is_first_canvas) printing_bracket = "(";
+    else                printing_bracket = "";
+    ccRes.Print((fileOutName + "_res.pdf" + printing_bracket).c_str(), "pdf");
+    ccPull.Print((fileOutName + "_pull.pdf" + printing_bracket).c_str(), "pdf");
+    is_first_canvas = false;
+  }
+
+  printing_bracket = "]";
+  TCanvas emptycanvas("emptycanvas", "", 1200, 800);
+  emptycanvas.Print((fileOutName + "_res.pdf" + printing_bracket).c_str(), "pdf");
+  emptycanvas.Print((fileOutName + "_pull.pdf" + printing_bracket).c_str(), "pdf");
+}
+
+template<typename T>
+  std::string to_string_with_precision(const T a_value, const int n) {
+  std::ostringstream out;
+  out.precision(n);
+  out << std::fixed << a_value;
+  return out.str();
+}
+
+HistoQuantities EvaluateHistoQuantities(const TH1* h) {
+  HistoQuantities result;
+  const float integral = h->GetEntries();
+  result.underflow_ = h->GetBinContent(0) / integral;
+  result.overflow_ = h->GetBinContent(h->GetNbinsX()+1) / integral;
+  result.mean_ = h->GetMean();
+  result.mean_err_ = h->GetMeanError();
+  result.stddev_ = h->GetStdDev();
+  result.stddev_err_ = h->GetStdDevError();
+
+  return result;
+}
+
+TPaveText ConverHistoQuantitiesToText(const HistoQuantities& q, float x1, float y1, float x2, float y2) {
+  TPaveText text(x1, y1, x2, y2, "brNDC");
+  text.SetFillColor(0);
+  text.SetTextSize(0.03);
+  text.SetTextFont(62);
+
+  text.AddText(("underflow = " + to_string_with_precision(q.underflow_*100, 2) + "%").c_str());
+  text.AddText(("overflow = " + to_string_with_precision(q.overflow_*100, 2) + "%").c_str());
+  text.AddText(("#mu = " + to_string_with_precision(q.mean_, 3) + " #pm " + to_string_with_precision(q.mean_err_, 3) + " (stat.)").c_str());
+  text.AddText(("#sigma = " + to_string_with_precision(q.stddev_, 3) + " #pm " + to_string_with_precision(q.stddev_err_, 3) + " (stat.)").c_str());
+
+  return text;
+}
+
+std::vector<std::pair<std::string, std::string>> FindCuts(const TFile* fileIn, const std::string& name_start) {
+  std::vector<std::pair<std::string, std::string>> result;
+
+  auto lok = fileIn->GetListOfKeys();
+  const int nDirs = lok->GetEntries();
+  for(int iDir=0; iDir<nDirs; iDir++) {
+    const std::string dirName = lok->At(iDir)->GetName();
+  }
+}
