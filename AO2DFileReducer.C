1beed7d4ef8c8425337a53db7d7e5a2e2906737d
Oleksii Lubynets <O.Lubynets@gsi.de>
2025-02-12 18:04:12 +0100

    on-fly change of AO2DFileReducer.C

--- AO2DFileReducer.C
+++ AO2DFileReducer.C
@@ -1,14 +1,14 @@
 void AO2DFileReducer() {
-  std::string fileInName = "/lustre/alice/users/lubynets/ao2ds/data/2022/LHC22o/526641/apass7/0630/o2_ctf_run00526641_orbit0206830848_tf0000000001_epn160/001/AO2D.root";
-  std::vector<std::string> dataFrameNames{"DF_2261906078563584", "DF_2261906078563840"};
+  std::string fileInName = "/lustre/alice/users/lubynets/CSTlc/outputs/mc/lhc24e3_tm/all/noConstr/AnalysisResults_trees.1.root";
+  std::vector<std::string> dataFrameNames{"DF_2261906081273110", "DF_2261906080459030", "DF_2261906080459158", "DF_2261906080459286", "DF_2261906080459414"};
   std::string fileOutName = "fileOut.root";
 
   TFile* fileIn = TFile::Open(fileInName.c_str(), "open");
   TFile* fileOut = TFile::Open(fileOutName.c_str(), "recreate");
 
-  TMap* tmap = (TMap*)fileIn->Get<TMap>("metaData")->Clone();
-  fileOut->cd();
-  tmap->Write("metaData", 1);
+//   TMap* tmap = (TMap*)fileIn->Get<TMap>("metaData")->Clone();
+//   fileOut->cd();
+//   tmap->Write("metaData", 1);
 
   for(auto& dFN : dataFrameNames) {
     TDirectoryFile* tdf = fileIn->Get<TDirectoryFile>(dFN.c_str());
@@ -19,6 +19,7 @@ void AO2DFileReducer() {
     TList* lok = tdf->GetListOfKeys();
     for(const auto& k : *lok) {
       std::string treeName = k->GetName();
+      std::cout << "treeName = " << treeName << "\n";
       TTree* tree = (TTree*)fileIn->Get<TTree>((dFN + "/" + treeName).c_str())->CloneTree();
       tree->Write();
     }
