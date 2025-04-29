6480f757bce99c480ae2af255777dd1723041ee5
Oleksii Lubynets <O.Lubynets@gsi.de>
2025-04-29 19:10:53 +0200

    [qa2] 1-st working version of bdt_efficiency

--- qa2/exe_based/tasks/bdt_efficiency.cpp
+++ qa2/exe_based/tasks/bdt_efficiency.cpp
@@ -28,14 +28,16 @@ class BdtEfficiencyCalculator {
   }
 
   void Run() {
-    for(int iBin=1; iBin<x_n_bins_; iBin++) {
-      for(int jBin=1; jBin<y_n_bins_; jBin++) {
-        histo_out_->SetBinContent(iBin, jBin, GetEfficiency(iBin, jBin));
+    for(int iBin=1; iBin<=x_n_bins_; iBin++) {
+      for(int jBin=1; jBin<=y_n_bins_; jBin++) {
+        const double eff = GetEfficiency(iBin, jBin);
+        histo_eff_->SetBinContent(iBin, jBin, eff);
+        histo_rej_->SetBinContent(iBin, jBin, 1. - eff);
       } // iBin
     } // jBin
   }
 
-  TH2D* GetEfficiencyHistogram() const { return histo_out_; }
+  TH2D* GetEfficiencyHistogram() const { return histo_eff_; }
 
  private:
   void Init() {
@@ -43,18 +45,26 @@ class BdtEfficiencyCalculator {
     y_n_bins_ = histo_in_->GetNbinsY();
     integral_ = histo_in_->Integral(1, x_n_bins_, 1, y_n_bins_);
 
-    histo_out_ = dynamic_cast<TH2D*>(histo_in_->Clone());
-    histo_out_->Reset();
+    histo_eff_ = dynamic_cast<TH2D*>(histo_in_->Clone());
+    histo_eff_->Reset();
     std::string xTitle = histo_in_->GetXaxis()->GetTitle();
     if(x_is_upper_cut_) xTitle += " upper";
     else                xTitle += " lower";
     xTitle += " value";
-    histo_out_->GetXaxis()->SetTitle(xTitle.c_str());
+    histo_eff_->GetXaxis()->SetTitle(xTitle.c_str());
     std::string yTitle = histo_in_->GetYaxis()->GetTitle();
     if(y_is_upper_cut_) yTitle += " upper";
     else                yTitle += " lower";
     yTitle += " value";
-    histo_out_->GetYaxis()->SetTitle(yTitle.c_str());
+    histo_eff_->GetYaxis()->SetTitle(yTitle.c_str());
+
+    histo_eff_->GetZaxis()->SetTitle("#varepsilon");
+    histo_eff_->GetZaxis()->SetTitleOffset(1.05);
+    histo_eff_->GetZaxis()->SetTitleSize(0.05);
+    histo_eff_->GetZaxis()->SetRangeUser(0, 1);
+
+    histo_rej_ = dynamic_cast<TH2D*>(histo_eff_->Clone());
+    histo_rej_->GetZaxis()->SetTitle("1 - #varepsilon");
   }
 
   double GetEfficiency(int binx, int biny) const {
@@ -62,15 +72,16 @@ class BdtEfficiencyCalculator {
     const int toX = x_is_upper_cut_ ? binx-1 : x_n_bins_;
     const int fromY = y_is_upper_cut_ ? 1 : biny+1;
     const int toY = y_is_upper_cut_ ? biny-1 : y_n_bins_;
-    const double integralInternal = histo_in_->Integral(fromX, toX, fromY, toY);
-    const double integralEdge = histo_in_->Integral(fromX, toX, biny, biny) / 2 +
-                                histo_in_->Integral(binx, binx, fromY, toY) / 2 +
-                                histo_in_->GetBinContent(binx, biny) / 4;
-    return (integralInternal + integralEdge) / integral_;
+    const double integralInternal = (toX > fromX && toY > fromY) ? histo_in_->Integral(fromX, toX, fromY, toY) : 0.;
+    const double integralEdgeVert = toY > fromY ? histo_in_->Integral(binx, binx, fromY, toY) / 2 : 0.;
+    const double integralEdgeHoriz = toX > fromX ? histo_in_->Integral(fromX, toX, biny, biny) / 2 : 0.;
+    const double integralCorner = histo_in_->GetBinContent(binx, biny) / 4;
+    return (integralInternal + integralEdgeVert + integralEdgeHoriz + integralCorner) / integral_;
   }
 
   const TH2* histo_in_;
-  TH2D* histo_out_;
+  TH2D* histo_eff_;
+  TH2D* histo_rej_;
   bool x_is_upper_cut_;
   bool y_is_upper_cut_;
   int x_n_bins_;
@@ -79,7 +90,7 @@ class BdtEfficiencyCalculator {
 };
 
 void BdtEfficiency(const std::string& fileNameMc, const std::string& fileNameData, const std::string& xCutDirection, const std::string& yCutDirection) {
-  LoadMacro("styles/mc_qa2.style.cc");
+  LoadMacro("styles/treeKF_qa2.style.cc");
   TFile* fileInMc = OpenFileWithNullptrCheck(fileNameMc);
   TFile* fileInData = OpenFileWithNullptrCheck(fileNameData);
 
@@ -98,6 +109,7 @@ void BdtEfficiency(const std::string& fileNameMc, const std::string& fileNameDat
     TH2D* histoBdtEff = bec.GetEfficiencyHistogram();
     TCanvas cc("cc", "");
     cc.SetCanvasSize(1000, 1000);
+    cc.SetRightMargin(0.17);
     histoBdtEff->Draw("colz");
     AddOneLineText(dt, {0.8, 0.95, 0.9, 0.99});
     cc.Print((fileOutName + ".pdf" + priBra).c_str(), "pdf");
