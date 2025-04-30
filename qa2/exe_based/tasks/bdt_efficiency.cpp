6cac794b18c8a5414f42f3c1a30cebbb5488c98f
Oleksii Lubynets <O.Lubynets@gsi.de>
2025-04-30 18:39:39 +0200

    [qa2] add sig eff ratio plot to bdt_efficiency

--- qa2/exe_based/tasks/bdt_efficiency.cpp
+++ qa2/exe_based/tasks/bdt_efficiency.cpp
@@ -112,7 +112,10 @@ void BdtEfficiency(const std::string& fileNameMc, const std::string& fileNameDat
   const std::string fileOutYieldName = "bdt_yield";
   const std::string fileOutEffName = "bdt_eff";
 
+  std::vector<BdtEfficiencyCalculator*> becs(dataTypes.size(), nullptr);
+
   std::string priBra = "(";
+  int iDt{0};
   for(const auto& dt : dataTypes) {
     TH2* histoBdt{nullptr};
     if(dt.is_mc_) histoBdt = GetObjectWithNullptrCheck<TH2>(fileInMc, dt.name_ + "/hBdt");
@@ -120,9 +123,9 @@ void BdtEfficiency(const std::string& fileNameMc, const std::string& fileNameDat
     histoBdt->UseCurrentStyle();
     histoBdt->GetZaxis()->SetTitleOffset(1.35);
 
-    BdtEfficiencyCalculator bec(histoBdt, xCutDirection, yCutDirection);
-    bec.Run();
-    TH2D* histoBdtOut = dt.is_oriented_ ? bec.GetEfficiencyHistogram() : bec.GetRejectionHistogram();
+    becs.at(iDt) = new BdtEfficiencyCalculator(histoBdt, xCutDirection, yCutDirection);
+    becs.at(iDt)->Run();
+    TH2D* histoBdtOut = dt.is_oriented_ ? becs.at(iDt)->GetEfficiencyHistogram() : becs.at(iDt)->GetRejectionHistogram();
     TCanvas ccEff("ccEff", "");
     TCanvas ccYield("ccYield", "");
     ccYield.SetLogz();
@@ -138,8 +141,19 @@ void BdtEfficiency(const std::string& fileNameMc, const std::string& fileNameDat
     ccYield.Print((fileOutYieldName + ".pdf" + priBra).c_str(), "pdf");
     ccEff.Print((fileOutEffName + ".pdf" + priBra).c_str(), "pdf");
     priBra = "";
-  }
+    ++iDt;
+  } // dataTypes
   CloseCanvasPrinting({fileOutYieldName, fileOutEffName});
+
+  TH2D* histoSignalEffRatio = dynamic_cast<TH2D*>(becs.at(1)->GetEfficiencyHistogram()->Clone()); // nonPrompt efficiency histo
+  histoSignalEffRatio->Divide(becs.at(0)->GetEfficiencyHistogram()); // divide by prompt efficiency
+  histoSignalEffRatio->GetZaxis()->SetTitle("#varepsilon_{nonPrompt} / #varepsilon_{prompt}");
+  histoSignalEffRatio->GetZaxis()->SetRangeUser(0.01, 1);
+  TCanvas ccRatio("ccRatio", "");
+  ccRatio.SetLogz();
+  ccRatio.SetCanvasSize(1000, 1000);
+  histoSignalEffRatio->Draw("colz");
+  ccRatio.Print("bdt_signal_ratio.pdf", "pdf");
 }
 
 
