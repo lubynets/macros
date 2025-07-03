9fc42a81a1ebdc04883868d916eeee1d8ee40bfe
Oleksii Lubynets <O.Lubynets@gsi.de>
2025-07-03 15:15:16 +0200

    [HFInvMassFitter] sync with O2Physics

--- qa2/exe_based/tasks/runMassFitter.C
+++ qa2/exe_based/tasks/runMassFitter.C
@@ -16,12 +16,18 @@
 /// \author Mingyu Zhang <mingyu.zang@cern.ch>
 /// \author Xinye Peng  <xinye.peng@cern.ch>
 /// \author Biao Zhang <biao.zhang@cern.ch>
+/// \author Oleksii Lubynets <oleksii.lubynets@cern.ch>
 
 #if !defined(__CINT__) || defined(__CLING__)
 
 #include "HFInvMassFitter.h"
 
 // if .h file not found, please include your local rapidjson/document.h and rapidjson/filereadstream.h here
+#include <TCanvas.h>
+#include <TDatabasePDG.h>
+#include <TFile.h>
+#include <TH2F.h>
+
 #include <rapidjson/document.h>
 #include <rapidjson/filereadstream.h>
 
@@ -29,11 +35,6 @@
 #include <stdexcept>
 #include <string> // std::string
 #include <vector> // std::vector
-
-#include <TDatabasePDG.h>
-#include <TFile.h>
-#include <TH2F.h>
-#include <TCanvas.h>
 
 #endif
 
@@ -674,11 +675,10 @@ void divideCanvas(TCanvas* canvas, int nSliceVarBins)
   }
 }
 
-int main(int argc, char* argv[]) {
-  if (argc < 2) {
-    printf("Error! Please use \n");
-    printf(" ./runMassFitter configFileName\n");
-    exit(EXIT_FAILURE);
+int main(int argc, char* argv[])
+{
+  if (argc == 1) {
+    throw std::runtime_error("Not enough arguments. Please use\n./runMassFitter configFileName");
   }
 
   const std::string configFileName = argv[1];
