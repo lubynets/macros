74c3b37249a1a34e1e43f3545982cc085eb5c1f9
Oleksii Lubynets <O.Lubynets@gsi.de>
2025-08-01 11:49:35 +0200

    [bdt] actualize bdtQAer, add definition of singularity for training

---
+++ bdt/bdtQAer.sh
@@ -0,0 +1,35 @@
+PLOTS=(
+'Basic_distributions'
+'BDTeff'
+'BDTprobBkg'
+'BDTprobNonPrompt'
+'BDTprobPrompt'
+'Chi2Geo_distributions'
+'Chi2Prim_distributions'
+'CorrMatrBkg'
+'CorrMatrNonPrompt'
+'CorrMatrPrompt'
+'DCA_distributions'
+'ImpactParameter_distributions'
+'NTpcTofSigma_distributions'
+'Others_distributions'
+'PreselEff'
+'ROCcurve'
+'shap_Bkg'
+'shap_NonPrompt'
+'shap_Prompt'
+'shapSummary'
+)
+
+for PL in ${PLOTS[@]}; do
+  echo "processing " $PL
+  convert 1/${PL}* 2/${PL}* 3/${PL}* 4/${PL}* 5/${PL}* $PL.pdf
+done
+
+for I in `seq 1 5`; do 
+  echo "processing pT interval " $I
+  cd $I
+  convert *png complexQA.Pt_$I.pdf
+  mv *pdf ../
+  cd ../
+done
