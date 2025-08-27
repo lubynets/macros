1bc7fce5ecce2a610761309f2837ed52550098a1
Oleksii Lubynets <O.Lubynets@gsi.de>
2025-08-27 18:34:43 +0200

    [QA, qa2] tpc add more histograms

--- QA/macro_based/tpc_qa.C
+++ QA/macro_based/tpc_qa.C
@@ -22,13 +22,24 @@ void tpc_qa(const std::string& fileName) {
   Float_t fTPCInnerParam;
   Float_t fBetaGamma;
   Float_t fTPCSignal;
+  Float_t fNSigTPC;
 
   std::array<TH2D*, Particles::nParticles> hPdEdx;
+  std::array<TH1D*, Particles::nParticles> hNSigma;
+  std::array<TH2D*, Particles::nParticles> hPNSigma;
 
   for(int kParticle=0; kParticle<Particles::nParticles; ++kParticle) {
     hPdEdx.at(kParticle) = new TH2D(("hPdEdx_" + particleNames.at(kParticle)).c_str(), particleNames.at(kParticle).c_str(), 100, 0.01, 10, 100, 0, 150);
     hPdEdx.at(kParticle)->GetXaxis()->SetTitle("#it{p} (GeV/#it{c})");
     hPdEdx.at(kParticle)->GetYaxis()->SetTitle("dE/dx (a.u.)");
+
+    hNSigma.at(kParticle) = new TH1D(("hNSigma_" + particleNames.at(kParticle)).c_str(), particleNames.at(kParticle).c_str(), 500, -10, 10);
+    hNSigma.at(kParticle)->GetXaxis()->SetTitle("N#sigma TPC");
+    hNSigma.at(kParticle)->GetYaxis()->SetTitle("Entries");
+
+    hPNSigma.at(kParticle) = new TH2D(("hPNSigma_" + particleNames.at(kParticle)).c_str(), particleNames.at(kParticle).c_str(), 100, 0.01, 10, 100, -10, 10);
+    hPNSigma.at(kParticle)->GetXaxis()->SetTitle("#it{p} (GeV/#it{c})");
+    hPNSigma.at(kParticle)->GetYaxis()->SetTitle("N#sigma TPC");
   }
 
   for(const auto& dirName : dirNames) {
@@ -38,6 +49,7 @@ void tpc_qa(const std::string& fileName) {
     treeIn->SetBranchAddress("fTPCInnerParam", &fTPCInnerParam);
     treeIn->SetBranchAddress("fBetaGamma", &fBetaGamma);
     treeIn->SetBranchAddress("fTPCSignal", &fTPCSignal);
+    treeIn->SetBranchAddress("fNSigTPC", &fNSigTPC);
 
     const int nEntries = treeIn->GetEntries();
     for(int iEntry=0; iEntry<nEntries; ++iEntry) {
@@ -45,8 +57,18 @@ void tpc_qa(const std::string& fileName) {
       const float p = fTPCInnerParam;
       const float beta = calcBetaFromBetaGamma(fBetaGamma);
       const float dEdx = fTPCSignal;
-      hPdEdx.at(GetParicleByfPidIndex(fPidIndex))->Fill(p, dEdx);
+      const float nsigma = fNSigTPC;
+
+      const short particleId = GetParicleByfPidIndex(fPidIndex);
+
+      hPdEdx.at(particleId)->Fill(p, dEdx);
       hPdEdx.at(Particles::kAll)->Fill(p, dEdx);
+
+      hNSigma.at(particleId)->Fill(nsigma);
+      hNSigma.at(Particles::kAll)->Fill(nsigma);
+
+      hPNSigma.at(particleId)->Fill(p, nsigma);
+      hPNSigma.at(Particles::kAll)->Fill(p, nsigma);
     }
 
     fileIn->Close();
@@ -55,6 +77,8 @@ void tpc_qa(const std::string& fileName) {
   TFile* fileOut = TFile::Open("tpc_qa.root", "recreate");
   for(int kParticle=0; kParticle<Particles::nParticles; ++kParticle) {
     hPdEdx.at(kParticle)->Write();
+    hNSigma.at(kParticle)->Write();
+    hPNSigma.at(kParticle)->Write();
   }
   fileOut->Close();
 }
--- qa2/macro_based/tpc_qa2.C
+++ qa2/macro_based/tpc_qa2.C
@@ -1,15 +1,27 @@
 const std::vector<std::string> particleNames{"proton", "pion", "electorn", "kaon", "all"};
+const std::vector<std::string> histoNames{"hPdEdx", "hNSigma", "hPNSigma"};
 
-void tpc_qa2(const std::string& fileName) {
+void tpc_qa2(const std::string& fileName, bool isParticleWise=true) {
   TFile* fileIn = TFile::Open(fileName.c_str(), "read");
   for(const auto& paName : particleNames) {
-    const std::string priBra = particleNames.size() == 1 ? "" : paName == particleNames.front() ? "(" : paName == particleNames.back() ? ")" : "";
-    TH1* h = fileIn->Get<TH1>(("hPdEdx_" + paName).c_str());
-    TCanvas cc("cc", "");
-    cc.SetCanvasSize(1200, 800);
-    cc.SetLogz();
-    h->Draw("colz");
-    cc.Print(("hPdEdx.pdf" + priBra).c_str(), "pdf");
+    for(const auto& hiName : histoNames) {
+      std::string priBra;
+      std::string ccName;
+      if(isParticleWise) {
+        priBra = histoNames.size() == 1 ? "" : hiName == histoNames.front() ? "(" : hiName == histoNames.back() ? ")" : "";
+        ccName = paName;
+      } else {
+        priBra = particleNames.size() == 1 ? "" : paName == particleNames.front() ? "(" : paName == particleNames.back() ? ")" : "";
+        ccName = hiName;
+      }
+      TH1* h = fileIn->Get<TH1>((hiName + "_" + paName).c_str());
+      TCanvas cc("cc", "");
+      cc.SetCanvasSize(1200, 800);
+      cc.SetLogx(std::string(h->GetXaxis()->GetTitle()) == "#it{p} (GeV/#it{c})");
+      cc.SetLogz();
+      h->Draw("colz");
+      cc.Print((ccName + ".pdf" + priBra).c_str(), "pdf");
+    }
   }
   fileIn->Close();
 }
