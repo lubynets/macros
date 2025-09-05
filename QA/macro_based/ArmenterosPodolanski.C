13dacb5f07c3ecb8b299f12a7e93eeee6ed0798e
Oleksii Lubynets <O.Lubynets@gsi.de>
2025-09-05 19:05:26 +0200

    [QA] ArmenterosPodolanski: eval ellipse params

--- QA/macro_based/ArmenterosPodolanski.C
+++ QA/macro_based/ArmenterosPodolanski.C
@@ -1,6 +1,9 @@
 // v0_ap_sim.C
 // A ROOT macro to simulate K_S^0 and Lambda decays and plot Armenteros-Podolanski (AP) variables
 
+double evalAlpha0(double M, double m1, double m2);
+double evalQStar(double M, double m1, double m2);
+double evalRalpha(double M, double m1, double m2, double P);
 
 void ArmenterosPodolanski(int nEvents = 10000000) {
     TRandom3 rng(0);
@@ -19,38 +22,43 @@ void ArmenterosPodolanski(int nEvents = 10000000) {
                         200, -1, 1, 200, 0, 0.3);
 
     auto doDecay = [&](double M, double m1, double m2, TH2D* h) {
-        for (int i = 0; i < nEvents; ++i) {
-            // Parent at high momentum along z
-            double pParent = rng.Uniform(10, 10); // GeV/c
-            TLorentzVector P(0, 0, pParent, sqrt(pParent*pParent + M*M));
+      std::cout << "alpha0 = " << evalAlpha0(M, m1, m2) << "\n";
+      std::cout << "QStar = " << evalQStar(M, m1, m2) << "\n";
+      std::cout << "evalRalpha(P=1) = " << evalRalpha(M, m1, m2, 1) << "\n";
+      std::cout << "evalRalpha(P=5) = " << evalRalpha(M, m1, m2, 5) << "\n";
+      std::cout << "evalRalpha(P=10) = " << evalRalpha(M, m1, m2, 10) << "\n";
+      for (int i = 0; i < nEvents; ++i) {
+          // Parent at high momentum along z
+          double pParent = rng.Uniform(10, 10); // GeV/c
+          TLorentzVector P(0, 0, pParent, sqrt(pParent*pParent + M*M));
 
-            // Daughter momenta in parent rest frame (two-body decay)
-            double pStar = TMath::Sqrt((M*M - (m1 + m2)*(m1 + m2)) * (M*M - (m1 - m2)*(m1 - m2))) / (2*M);
-            double costh = rng.Uniform(-1, 1);
-            double phi = rng.Uniform(0, 2*TMath::Pi());
-            TLorentzVector p1_rf(
-                pStar * TMath::Sin(acos(costh)) * TMath::Cos(phi),
-                pStar * TMath::Sin(acos(costh)) * TMath::Sin(phi),
-                pStar * costh,
-                sqrt(pStar*pStar + m1*m1)
-            );
-            TLorentzVector p2_rf(-p1_rf.Vect(), sqrt(pStar*pStar + m2*m2));
+          // Daughter momenta in parent rest frame (two-body decay)
+          double pStar = TMath::Sqrt((M*M - (m1 + m2)*(m1 + m2)) * (M*M - (m1 - m2)*(m1 - m2))) / (2*M);
+          double costh = rng.Uniform(-1, 1);
+          double phi = rng.Uniform(0, 2*TMath::Pi());
+          TLorentzVector p1_rf(
+              pStar * TMath::Sin(acos(costh)) * TMath::Cos(phi),
+              pStar * TMath::Sin(acos(costh)) * TMath::Sin(phi),
+              pStar * costh,
+              sqrt(pStar*pStar + m1*m1)
+          );
+          TLorentzVector p2_rf(-p1_rf.Vect(), sqrt(pStar*pStar + m2*m2));
 
-            // Boost daughters to lab frame
-            TVector3 beta = P.BoostVector();
-            p1_rf.Boost(beta);
-            p2_rf.Boost(beta);
+          // Boost daughters to lab frame
+          TVector3 beta = P.BoostVector();
+          p1_rf.Boost(beta);
+          p2_rf.Boost(beta);
 
-            double pL1 = p1_rf.Vect().Dot(P.Vect().Unit());
-            double pL2 = p2_rf.Vect().Dot(P.Vect().Unit());
-            double pT1 = p1_rf.Vect().Perp(P.Vect().Unit());
-            // pT2 is the same by momentum conservation
+          double pL1 = p1_rf.Vect().Dot(P.Vect().Unit());
+          double pL2 = p2_rf.Vect().Dot(P.Vect().Unit());
+          double pT1 = p1_rf.Vect().Perp(P.Vect().Unit());
+          // pT2 is the same by momentum conservation
 
-            double alpha = (pL1 - pL2) / (pL1 + pL2);
-            double pT = pT1;
+          double alpha = (pL1 - pL2) / (pL1 + pL2);
+          double pT = pT1;
 
-            h->Fill(alpha, pT);
-        }
+          h->Fill(alpha, pT);
+      }
     };
 
 //     doDecay(mK0, mpi, mpi, hAP);
@@ -66,4 +74,17 @@ void ArmenterosPodolanski(int nEvents = 10000000) {
     c->SetLogz();
 
     c->Print("AP.pdf", "pdf");
+}
+
+double evalAlpha0(double M, double m1, double m2) {
+  return std::fabs(m1*m1 - m2*m2) / M / M;
+}
+
+double evalQStar(double M, double m1, double m2) {
+  return std::sqrt(M*M*M*M + m1*m1*m1*m1 + m2*m2*m2*m2 + - 2.*M*M*(m1*m1 + m2*m2) - 2.*m1*m1*m2*m2) / 2. / M;
+}
+
+double evalRalpha(double M, double m1, double m2, double P) {
+  const float b = P / std::sqrt(P*P + M*M);
+  return 2*evalQStar(M, m1, m2) / b / M;
 }
