e2e7291099c078d89cbe326a2d2ef96fc2ad6a8a
Oleksii Lubynets <O.Lubynets@gsi.de>
2025-12-12 19:08:20 +0100

    [qa2] mass_bdt_qa_thn: foresee RunOnly, RunAndMerge, MergeOnly

--- qa2/exe_based/tasks/mass_bdt_qa_thn.cpp
+++ qa2/exe_based/tasks/mass_bdt_qa_thn.cpp
@@ -13,10 +13,10 @@
 
 using namespace HelperGeneral;
 
-const std::vector<float> lifetimeRanges = {0.2, 0.35, 0.5, 0.7, 0.9, 1.6};
+const std::vector<float> lifetimeRanges = {0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0};
 const std::string lifetimeAxisTitle = "T_{proper} (ps)";
 
-const std::vector<float> pTRanges = {1, 3, 5, 8, 12, 20};
+std::vector<float> pTRanges = {1, 3, 5, 8, 12, 20};
 const std::string pTAxisTitle = "#it{p}_{T}(#Lambda_{c}^{+}) (GeV/#it{c})";
 
 const std::vector<float> bdtBgUpperValuesVsPt = {0.02, 0.02, 0.02, 0.05, 0.08};
@@ -26,20 +26,31 @@ const std::string massAxisTitle = "inv. mass (p K #pi) (GeV/#it{c}^{2})";
 
 std::string GetPtCutName(size_t iPt);
 
+enum : int {
+  RunOnly = 0,
+  RunAndMerge,
+  MergeOnly,
+  NModeRuns
+};
 
-void MassBdtQaThn(const std::string& fileNameIn) {
+void MassBdtQaThn(const std::string& fileNameIn, int modeRun) {
 //  LoadMacro("styles/mc_qa2.style.cc");
   const std::string fileName = ReadNthLine(fileNameIn);
 
-  TFile* fileIn = OpenFileWithNullptrCheck(fileName);
-  TFile* fileOut = TFile::Open("mass_bdt_qa_thn.root", "recreate");
+  TFile* fileIn = modeRun != MergeOnly ? OpenFileWithNullptrCheck(fileName) : nullptr;
+  const std::string fileOutName = modeRun != MergeOnly ? "mass_bdt_qa_thn.root" : fileNameIn;
+  const std::string fileOutOption = modeRun != MergeOnly ? "recreate" : "update";
+  TFile* fileOut = TFile::Open(fileOutName.c_str(), fileOutOption.c_str());
 
-  THnSparse* histoIn = GetObjectWithNullptrCheck<THnSparse>(fileIn, "hf-task-lc/hnLcVarsWithBdt");
-  const std::map<std::string, int> axesIndices = MapAxesIndices(histoIn);
+  THnSparse* histoIn = modeRun != MergeOnly ? GetObjectWithNullptrCheck<THnSparse>(fileIn, "hf-task-lc/hnLcVarsWithBdt") : nullptr;
 
-  CheckTAxisForRanges(*histoIn->GetAxis(axesIndices.at(pTAxisTitle)), pTRanges);
-  CheckTAxisForRanges(*histoIn->GetAxis(axesIndices.at(bgAxisTitle)), bdtBgUpperValuesVsPt);
-  CheckTAxisForRanges(*histoIn->GetAxis(axesIndices.at(lifetimeAxisTitle)), lifetimeRanges);
+  const std::map<std::string, int> axesIndices = modeRun != MergeOnly ? MapAxesIndices(histoIn) : std::map<std::string, int>{};
+  if(modeRun != MergeOnly) {
+    CheckTAxisForRanges(*histoIn->GetAxis(axesIndices.at(pTAxisTitle)), pTRanges);
+    CheckTAxisForRanges(*histoIn->GetAxis(axesIndices.at(bgAxisTitle)), bdtBgUpperValuesVsPt);
+    CheckTAxisForRanges(*histoIn->GetAxis(axesIndices.at(lifetimeAxisTitle)), lifetimeRanges);
+  }
+
   if(bdtBgUpperValuesVsPt.size() != pTRanges.size() - 1) throw std::runtime_error("bdtUpperValuesVsPt.size() != pTRanges.size() - 1");
 
   std::vector<float> bdtSignalLowerValues;
@@ -54,7 +65,7 @@ void MassBdtQaThn(const std::string& fileNameIn) {
     tCutNames.emplace_back("T_" + to_string_with_precision(lifetimeRanges.at(iT), 2) + "_" + to_string_with_precision(lifetimeRanges.at(iT+1), 2));
   }
 
-  for(size_t iPt=0, nPts=pTRanges.size()-1; iPt<nPts; ++iPt) {
+  for(size_t iPt=0, nPts=pTRanges.size()-1; iPt<nPts && modeRun != MergeOnly; ++iPt) {
     std::cout << "\nProcessing iPt = " << iPt << "\n";
     SetTHnSparseAxisRanges(histoIn, axesIndices.at(pTAxisTitle), pTRanges.at(iPt), pTRanges.at(iPt + 1));
     SetTHnSparseAxisRanges(histoIn, axesIndices.at(bgAxisTitle), 0., bdtBgUpperValuesVsPt.at(iPt));
@@ -80,21 +91,23 @@ void MassBdtQaThn(const std::string& fileNameIn) {
     SetTHnSparseAxisRanges(histoIn, axesIndices.at(bgAxisTitle));
   } // pTRanges
 
-  for (const auto& tcn : tCutNames) {
-    for (const auto& bslv : bdtSignalLowerValues) {
-      std::vector<std::string> histoNames;
-      histoNames.reserve(pTCutNames.size());
-      for (const auto& ptcn : pTCutNames) {
-        histoNames.emplace_back(ptcn + "/" + tcn + "/hM_NPgt" + to_string_with_precision(bslv, 2));
-      }
-      TH1* histoMerged = HelperMath::MergeHistograms(fileOut, histoNames);
-      HelperGeneral::CD(fileOut, GetPtCutName(pTRanges.size()-1) + "/" + tcn);
-      histoMerged->Write(("hM_NPgt" + to_string_with_precision(bslv, 2)).c_str());
-    } // bdtSignalLowerValues
-  } // TCuts
+  if(modeRun != RunOnly) {
+    for (const auto& tcn : tCutNames) {
+      for (const auto& bslv : bdtSignalLowerValues) {
+        std::vector<std::string> histoNames;
+        histoNames.reserve(pTCutNames.size());
+        for (const auto& ptcn : pTCutNames) {
+          histoNames.emplace_back(ptcn + "/" + tcn + "/hM_NPgt" + to_string_with_precision(bslv, 2));
+        }
+        TH1* histoMerged = HelperMath::MergeHistograms(fileOut, histoNames);
+        HelperGeneral::CD(fileOut, GetPtCutName(pTRanges.size()-1) + "/" + tcn);
+        histoMerged->Write(("hM_NPgt" + to_string_with_precision(bslv, 2)).c_str());
+      } // bdtSignalLowerValues
+    } // TCuts
+  }
 
   fileOut->Close();
-  fileIn->Close();
+  if(modeRun != MergeOnly) fileIn->Close();
 }
 
 std::string GetPtCutName(size_t iPt) {
@@ -105,13 +118,15 @@ std::string GetPtCutName(size_t iPt) {
 int main(int argc, char* argv[]) {
   if (argc < 2) {
     std::cout << "Error! Please use " << std::endl;
-    std::cout << " ./mass_bdt_qa_thn fileNameIn" << std::endl;
+    std::cout << " ./mass_bdt_qa_thn fileNameIn (modeRun=0)" << std::endl;
     exit(EXIT_FAILURE);
   }
 
   const std::string fileNameIn = argv[1];
+  const int modeRun = argc > 2 ? std::stoi(argv[2]) : RunAndMerge;
+  if(modeRun < 0 || modeRun >= NModeRuns) throw std::runtime_error("modeRun < 0 || modeRun >= NModeRuns");
 
-  MassBdtQaThn(fileNameIn);
+  MassBdtQaThn(fileNameIn, modeRun);
 
   return 0;
 }
