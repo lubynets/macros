f73821ff7771e47658283773dc26b3721822ca4d
Oleksii Lubynets <O.Lubynets@gsi.de>
2026-02-17 18:40:07 +0100

    [qa2] otf th2ToResidualVsX

--- qa2/macro_based/th2ToResidualVsX.C
+++ qa2/macro_based/th2ToResidualVsX.C
@@ -3,7 +3,10 @@ int FindWhoseTH1LowerEdge(TAxis* histo, double value);
 void th2ToResidualVsX(const std::string& fileName, const std::string& histoName) {
   gROOT->Macro("/home/oleksii/alidir/macros_on_git/qa2/exe_based/styles/mc_qa2.style.cc");
 
-  const std::array binEdges{0.2, 0.4, 0.6, 0.8, 1.0, 1.4, 1.8};
+  std::vector<double> binEdges;
+  for(int iStep=0; iStep<=40; ++iStep) {
+    binEdges.emplace_back(iStep * 0.05);
+  }
 
   TFile* fileIn = TFile::Open(fileName.c_str(), "read");
   if(fileIn == nullptr) throw std::runtime_error("File " + fileName + " is missing");
@@ -11,7 +14,8 @@ void th2ToResidualVsX(const std::string& fileName, const std::string& histoName)
   TH2* histoIn = fileIn->Get<TH2>(histoName.c_str());
   if(histoIn == nullptr) throw std::runtime_error("Histogram " + histoName + " is missing");
 
-  TGraphErrors grRes("grRes");
+  TGraphErrors grRes;
+  grRes.SetName("grRes");
   grRes.GetXaxis()->SetTitle(histoIn->GetXaxis()->GetTitle());
   grRes.GetYaxis()->SetTitle(("std. dev. of "s + histoIn->GetYaxis()->GetTitle()).c_str());
 
