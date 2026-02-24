c36c88dab9a8473982a59d046e1419a17851bc4d
Oleksii Lubynets <O.Lubynets@gsi.de>
2026-02-24 19:40:30 +0100

    [Other] otf ctReso

--- Other/ctReso/th1tf1SmearConsistency.C
+++ Other/ctReso/th1tf1SmearConsistency.C
@@ -1,3 +1,7 @@
+constexpr double PI{3.141592654};
+
+TH1* EvaluateEfficiency(const TH1* histoNum, const TH1* histoDen);
+
 class ExpoSmearFunction {
 public:
   void Init() {
@@ -7,27 +11,43 @@ public:
     is_init_ = true;
   }
 
+  void SetMeanContainer(TGraph* graph) { graph_mean_container_ = graph; }
   void SetSigmaContainer(TGraph* graph) { graph_sigma_container_ = graph; }
+  void SetEfficiencyGenHistogram(TH1* histo) { efficiency_gen_based_ = histo; }
+  void SetEfficiencyRecHistogram(TH1* histo) { efficiency_rec_based_ = histo; }
 
   double operator()(double* xx, double* par) {
     if(!is_init_) Init();
     double result{};
     const double x = xx[0];
-    double sigma{};
-    if(x < graph_sigma_container_->GetX()[0]) {
-      sigma = graph_sigma_container_->GetY()[0];
-    } else if (x > graph_sigma_container_->GetX()[graph_sigma_container_->GetN()-1]) {
-      sigma = graph_sigma_container_->GetY()[graph_sigma_container_->GetN()-1];
-    } else {
-      sigma = graph_sigma_container_->Eval(x);
-    }
-    func_smear_->SetParameters(1/std::sqrt(2*3.1415)/sigma, 0, sigma);
+
     func_orig_->SetParameters(par[0], par[1]);
+
+    auto ReadValueFromGraph = [](TGraph* graph, double arg) {
+      if(arg < graph->GetX()[0]) {
+        return  graph->GetY()[0];
+      } else if(arg > graph->GetX()[graph->GetN()-1]) {
+        return  graph->GetY()[graph->GetN()-1];
+      } else {
+        return  graph->Eval(arg);
+      }
+    };
+
     for(int iPoint=0; iPoint<numer_of_smearing_points_; ++iPoint) {
-      const double diffFromCenter = -smear_range_ + iPoint*step_;
-      const double funcValue = func_orig_->Eval(x + diffFromCenter);
-      const double smearFactor = func_smear_->Eval(diffFromCenter);
-      result += funcValue * smearFactor;
+      const double shiftFromPointToBeFilled = -smear_range_ + iPoint*step_;
+      const double pointToBeTakenFrom = x + shiftFromPointToBeFilled;
+
+      const double mean = ReadValueFromGraph(graph_mean_container_, pointToBeTakenFrom);
+      const double sigma = ReadValueFromGraph(graph_sigma_container_, pointToBeTakenFrom);
+      const double effGen = efficiency_gen_based_->Interpolate(pointToBeTakenFrom);
+      const double effRec = efficiency_rec_based_->Interpolate(x);
+
+      const double funcValue = func_orig_->Eval(pointToBeTakenFrom);
+
+      func_smear_->SetParameters(1/std::sqrt(2*PI)/sigma, 0, sigma);
+
+      const double smearFactor = func_smear_->Eval(shiftFromPointToBeFilled + mean);
+      result += funcValue * smearFactor * effGen / effRec;
     }
     result *= step_;
     return result;
@@ -35,8 +55,11 @@ public:
 
 private:
   TF1* func_orig_{nullptr};
+  TGraph* graph_mean_container_{nullptr};
   TGraph* graph_sigma_container_{nullptr};
   TF1* func_smear_{nullptr};
+  TH1* efficiency_gen_based_{nullptr};
+  TH1* efficiency_rec_based_{nullptr};
   double smear_range_{1.};
   double step_{};
   int numer_of_smearing_points_{1000};
@@ -45,10 +68,22 @@ private:
 
 void th1tf1SmearConsistency() {
   gStyle->SetHistLineWidth(2);
-  TFile* fileReso = TFile::Open("grRes.root", "read");
+  TFile* fileReso = TFile::Open("grRes/grRes.prompt.unsigned.root", "read");
   if(fileReso == nullptr) throw std::runtime_error("fileReso == nullptr");
-  TGraph* grReso = fileReso->Get<TGraph>("grRes");
-  if(grReso == nullptr) throw std::runtime_error("grReso == nullptr");
+  TGraph* grResoMean = fileReso->Get<TGraph>("mean");
+  if(grResoMean == nullptr) throw std::runtime_error("grResoMean == nullptr");
+  TGraph* grResoSigma = fileReso->Get<TGraph>("sigma");
+  if(grResoSigma == nullptr) throw std::runtime_error("grResoSigma == nullptr");
+
+  TFile* fileYield = TFile::Open("ct_yield_qa.HF_LHC24h1b_All.595984.root", "read");
+  if(fileYield == nullptr) throw std::runtime_error("fileYield == nullptr");
+  TH1* hYieldGen = fileYield->Get<TH1>("prompt/hGen");
+  TH1* hYieldCand = fileYield->Get<TH1>("prompt/hCand");
+  TH1* hYieldSim = fileYield->Get<TH1>("prompt/hSim");
+  if(hYieldGen == nullptr || hYieldCand == nullptr || hYieldSim == nullptr) throw std::runtime_error("hYieldGen == nullptr || hYieldCand == nullptr || hYieldSim == nullptr");
+
+  TH1* hEffSim = EvaluateEfficiency(hYieldSim, hYieldGen);
+  TH1* hEffCand = EvaluateEfficiency(hYieldCand, hYieldGen);
 
   const int nBins{400};
   const double lo{0.};
@@ -57,40 +92,61 @@ void th1tf1SmearConsistency() {
 
   const double tau{0.2};
 
-  const int nFills{100000000};
+  const int nFills{10000000};
   TH1* hSmeared = new TH1D("hSmeared", "", nBins, lo, hi);
   for(int iFill=0; iFill<nFills; ++iFill) {
+    if(iFill%(nFills/20) == 0) std::cout << "iFill = " << iFill << "\n";
     const double smearCentralValue = gRandom->Exp(tau);
-    double sigma{};
-    if(smearCentralValue < grReso->GetX()[0]) {
-      sigma = grReso->GetY()[0];
-    } else if(smearCentralValue > grReso->GetX()[grReso->GetN()-1]) {
-      sigma = grReso->GetY()[grReso->GetN()-1];
-    } else {
-      sigma = grReso->Eval(smearCentralValue);
-    }
+    if(gRandom->Uniform(1) > hEffSim->GetBinContent(hEffSim->FindBin(smearCentralValue))) continue;
+
+    auto ReadValueFromGraph = [&](TGraph* graph) {
+      if(smearCentralValue < graph->GetX()[0]) {
+        return  graph->GetY()[0];
+      } else if(smearCentralValue > graph->GetX()[graph->GetN()-1]) {
+        return  graph->GetY()[graph->GetN()-1];
+      } else {
+        return  graph->Eval(smearCentralValue);
+      }
+    };
+    const double mean = ReadValueFromGraph(grResoMean);
+    const double sigma = ReadValueFromGraph(grResoSigma);
+
     const double smearShiftValue = gRandom->Gaus(0, sigma);
     hSmeared->Fill(smearCentralValue + smearShiftValue);
   }
+
+  hSmeared->Sumw2();
+  hSmeared->Divide(hEffCand);
 
   hSmeared->SetLineColor(kBlue);
 
   //================================================================================================
 
   ExpoSmearFunction fitFunctorSmeared;
-  fitFunctorSmeared.SetSigmaContainer(grReso);
+  fitFunctorSmeared.SetMeanContainer(grResoMean);
+  fitFunctorSmeared.SetSigmaContainer(grResoSigma);
+  fitFunctorSmeared.SetEfficiencyGenHistogram(hEffSim);
+  fitFunctorSmeared.SetEfficiencyRecHistogram(hEffCand);
   fitFunctorSmeared.Init();
 
-  TF1* fitFuncSmeared = new TF1("fitFuncSmeared", fitFunctorSmeared, 0., 2., 2);
+  TF1* fitFuncSmeared = new TF1("fitFuncSmeared", fitFunctorSmeared, 0.0, 2.0, 2);
   fitFuncSmeared->SetNpx(1000);
 
   const double A = nFills * binWidth / tau / (std::exp(-lo/tau) - std::exp(-hi/tau));
   fitFuncSmeared->SetParameters(A, tau);
 
 //   hSmeared->Draw();
-//   fitFuncSmeared->Draw("same");
+  fitFuncSmeared->Draw();
 
-  hSmeared->Divide(fitFuncSmeared);
-  hSmeared->Draw();
+//   hSmeared->Divide(fitFuncSmeared);
+//   hSmeared->Draw();
 
+}
+
+TH1* EvaluateEfficiency(const TH1* histoNum, const TH1* histoDen) {
+  TH1* histoEff = dynamic_cast<TH1*>(histoNum->Clone());
+  histoEff->Sumw2();
+  histoEff->Divide(histoDen);
+
+  return histoEff;
 }
